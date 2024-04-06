e6114b1f19b4ed6e097602303f7dc255634379b7
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-04-06 11:12:58 -0700

    Add class vs inheritance experiment

---
+++ experiments/class-vs-construct.js
@@ -0,0 +1,59 @@
+const itr = [1000, 10000, 100000, 1000000]
+
+const create_class = class {
+    constructor(val) {
+        this.val = val
+    }
+}
+
+const create_obj = (val) => ({ val })
+
+// itr.forEach(n => {
+//     const label_class = `create class (${n}): `
+//     const start_class = performance.now()
+//     for (let i = 0; i < n; ++i) 
+//         new create_class(i)
+//     const time_class = performance.now() - start_class
+//     console.log(label_class + time_class)
+
+//     const label_construct = `create construct (${n}): `
+//     const start_construct = performance.now()
+//     for (let i = 0; i < n; ++i)
+//         create_obj(i)
+//     const time_construct = performance.now() - start_construct
+//     console.log(label_construct + time_construct)
+// })
+
+let t = 100
+const res_class = new Array(itr.length).fill(0)
+const res_obj = new Array(itr.length).fill(0)
+const test = () => {
+    while (t > 0) {
+        itr.forEach((n, i) => {
+            const start_class = performance.now()
+            for (let i = 0; i < n; ++i) 
+                new create_class(i)
+            res_class[i] += performance.now() - start_class
+        })
+        
+        itr.forEach((n, i) => {
+            const start_construct = performance.now()
+            for (let i = 0; i < n; ++i)
+                create_obj(i)
+            res_obj[i] += performance.now() - start_construct
+        })
+
+        t--
+    }
+
+    res_class.map((res, i) => {
+        const avg = res / 100
+        console.log(`create class (${itr[i]}): ${avg}`)
+    })
+
+    res_obj.map((res, i) => {
+        const avg = res / 100
+        console.log(`create obj (${itr[i]}): ${avg}`)
+    })
+}
+test()
